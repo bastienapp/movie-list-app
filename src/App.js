709271aebf28b2cb64e8f429ebf67e4a9549afe9
bastienwcs <bastien.krettly@wildcodeschool.com>
2020-11-16 17:27:15 +0100

    router init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import "./App.css";
 import MovieList from "./components/MovieList";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <MovieList />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route exact path="/movies">
+            <MovieList />
+          </Route>
+          <Route exact path="/" />
+          <Redirect to="/movies" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
